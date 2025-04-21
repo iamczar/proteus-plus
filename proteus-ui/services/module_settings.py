8aa0c96f45d872b3484075ea872597a0bc4da067
pcarry <pcarrillo92@outlook.com>
2025-04-21 17:35:12 +0100

    refactor module selector

--- proteus-ui/services/module_settings.py
+++ proteus-ui/services/module_settings.py
@@ -8,24 +8,36 @@ def update_module_id():
 
 
 def scan_for_modules():
-    show_toast("Scanning for modules...", "info")
+    # show_toast("Scanning for modules...", "info")
+    st.rerun(scope="app")
 
 
-@st.dialog("Select Module ID")
-def select_module_dialog():
-    st.write(f'Active module ID is {f'**{st.session_state.module_id}**'}')
+@st.dialog("Change Module ID")
+def select_module_dialog(current_module_id):
+    st.write(f'Active module ID is {f'**{current_module_id}**'}')
     # Operation Mode with session state
     available_modules = [None, 1001, 3006, 2501, 2502]
-    selected_mode = st.selectbox(
-        "Select Module ID",
-        available_modules,
-        key="temp_module_id",
-        on_change=update_module_id,
-        index=available_modules.index(st.session_state.module_id)
-    )
-
-    st.button("Scan for Modules", on_click=scan_for_modules)
-    st.rerun()
+    col1, col2 = st.columns([2, 1])
+    with col1:
+        selected_mode = st.selectbox(
+            label="Select Module ID",
+            options=available_modules,
+            key="temp_module_id",
+            on_change=update_module_id,
+            index=available_modules.index(st.session_state.module_id)
+        )
+    with col2:
+        # Insert a tall empty space
+        st.markdown(
+            "<div style='height:28px;'></div>",
+            unsafe_allow_html=True
+        )
+        if st.button("Select",
+                     on_click=scan_for_modules,
+                     use_container_width=True,
+                     type="primary",
+                     disabled=selected_mode is None):
+            st.rerun()
 
 
 @st.dialog("System Shutdown")
@@ -51,7 +63,8 @@ def render_module_settings():
     st.sidebar.header("Module Settings")
     st.sidebar.metric("Active Module", st.session_state.module_id)
 
-    st.sidebar.button("Change Module ID", on_click=select_module_dialog, type="primary")
+    current_module_id = st.session_state.module_id
+    st.sidebar.button("Change Module ID", on_click=select_module_dialog, args=[current_module_id], type="secondary")
 
     # Return current settings
     return {
