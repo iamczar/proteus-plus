f1d7ae49ecd58c43dc3cfea1f62580c7fbab6326
czar <czarbalangue@gmail.com>
2025-09-05 02:10:01 +0100

    wip

--- module_controller/module_handler.py
+++ module_controller/module_handler.py
@@ -462,6 +462,10 @@ class ModuleHandler:
                 # All other data_logger system events go to sys-logger
                 return f"sys-logger/{self.module_id}"
 
+            # - data_logger_ack -> sys-logger (explicit maintenance/status acks)
+            if source == "data_logger_ack":
+                return f"sys-logger/{self.module_id}"
+
             # - SysLogger -> sys-logger
             if source == "syslogger":
                 return f"sys-logger/{self.module_id}"
