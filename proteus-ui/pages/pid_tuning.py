440a3acb0c22ac6bacd2e409c85bff87d6472fd9
czar <czarbalangue@gmail.com>
2025-09-20 14:33:49 +0100

    wip

--- proteus-ui/pages/pid_tuning.py
+++ proteus-ui/pages/pid_tuning.py
@@ -223,13 +223,10 @@ st.markdown(
 
 @st.fragment(run_every=0.5)
 def _logs_tick():
-    header_cols = st.columns([1, 8], gap="small")
-    with header_cols[0]:
-        with st.form("pt_clear_logs_form"):
-            if st.form_submit_button("Clear"):
-                st.session_state.pt_logs = []
-    with header_cols[1]:
-        st.subheader("PID Tuning Logs")
+    st.subheader("PID Tuning Logs")
+    with st.form("pt_clear_logs_form"):
+        if st.form_submit_button("Clear"):
+            st.session_state.pt_logs = []
     log_content = "\n".join(st.session_state.get("pt_logs", [])[-400:])
     st.markdown(f"<div class='pt-log-box'>{log_content}</div>", unsafe_allow_html=True)
 
