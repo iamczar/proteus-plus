3d0615a06203f554426ce4c25983da5f41277664
root <root@eva.localdomain>
2025-11-13 11:17:34 +0000

    enable module controller

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,13 +7,13 @@ module.exports = {
       cwd: __dirname + "/proteus-ui",
       autorestart: false
     },
-    // {
-    //   name: "module-controller",
-    //   script: "./venv/Scripts/python.exe",
-    //   args: "-m module_controller",
-    //   cwd: __dirname,
-    //   autorestart: true,
-    //   max_restarts: 5
-    // }
+    {
+      name: "module-controller",
+      script: "./venv/Scripts/python.exe",
+      args: "-m module_controller",
+      cwd: __dirname,
+      autorestart: true,
+      max_restarts: 5
+    }
   ]
 };
